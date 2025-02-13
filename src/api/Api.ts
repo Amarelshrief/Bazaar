79a9752a2d1e6a21d9bdd860672dabf5931e7625
Amarelshrief <amarelshrief511@gmail.com>
2025-02-13 14:29:22 +0200

    Editing 'Api.ts' to edit fetching data

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
 export async function ProductsData() {
-  return await axios.get("https://fakestoreapiserver.reactbd.com/products");
+  const data = await axios.get(
+    "https://fakestoreapiserver.reactbd.com/products"
+  );
+  // console.log(data);
+
+  // const error = new Error("An error occurred while fetching the events");
+  return data.data;
 }
